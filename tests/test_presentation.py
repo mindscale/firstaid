baf4d8d9b7645db2f913a8d3d4166e120cc25f1d
yirey <yirey@naver.com>
2018-03-29 14:50:14 +0900

    설명서용 테스트 만들기

--- tests/test_presentation.py
+++ tests/test_presentation.py
@@ -1,7 +1,7 @@
 from itertools import combinations
 
 import pandas as pd
-
+import pytest
 from firstaid.pptx import PPTX
 
 
@@ -19,13 +19,13 @@ def test_bulletpoint2():
 
 def test_image1():
     p = PPTX()
-    p.add('This is a test', 'tests/image.png')
+    p.add('This is a test', 'tests/icecream.png')
     p.save('tests/presentation_test_results/test_image1.pptx')
 
 
 def test_image2():
     p = PPTX()
-    p.add('This is a test', 'tests/image.png', 'tests/image.png')
+    p.add('This is a test', 'tests/icecream.png', 'tests/icecream.png')
     p.save('tests/presentation_test_results/test_image2.pptx')
 
 
@@ -65,7 +65,7 @@ def test_plot2():
 
 def test_mixed_combinations():
     bullet = ['first bullet', 'second bullet']
-    img = 'tests/image.png'
+    img = 'tests/icecream.png'
     df = pd.DataFrame([[1, 2, 3], [4, 5, 6]], columns=['a', 'b', 'c'], index=['남', '여'])
     d = {'df': df, 'chart_type': 'line'}
 
@@ -76,3 +76,44 @@ def test_mixed_combinations():
     for i, (left, right) in enumerate(comb):
         p.add('This is a test', left, right)
     p.save('tests/presentation_test_results/test_combination.pptx')
+
+
+@pytest.mark.xfail(run=False)
+def test_getting_started_pics():
+    my_file = PPTX()
+    my_file.add(title='제목입니다',
+                content1=['첫 문장입니다', '두번째 문장입니다'])
+    my_file.add(title='제목입니다',
+                content1=['첫 문장입니다', '두번째 문장입니다'],
+                content2=['first sentence', 'second sentence'])
+    my_file.add('제목입니다',
+                ['첫 문장입니다', '두번째 문장입니다'],
+                ['first sentence', 'second sentence'])
+    points = ['1. 첫 문장 입니다',
+              '2. 두번째 문장입니다',
+              '3. 세번째 문장입니다']
+    my_file.add('문자열 슬라이드입니다',
+                points)
+    my_file.add('이미지 슬라이드입니다',
+                'tests/icecream.png')
+    df = pd.DataFrame([[10, 30, 50], [24, 13, 70]],
+                      columns=['A사', 'B사', 'C사'],
+                      index=['남', '여'])
+    my_file.add('표 슬라이드 입니다', df)
+    chart_dict = {'df': df,  # pandas DataFrame 형식
+                  'chart_type': 'line'}  # 'bar' 도 가능하다
+    my_file.add('차트 슬라이드 입니다',
+                chart_dict)
+
+    my_file.add('내가 좋아하는 디저트',
+                ['아이스크림', '초콜릿'],  # 문자열
+                'tests/icecream.png')  # 이미지
+    my_file.add('동물들의 발자국',
+                ['동물들의 발자국을 공부해봅시다'])
+    # 두번째 슬라이드
+    my_file.add('개', 'tests/dog.png')
+    # 세번째 슬라이드
+    my_file.add('말', 'tests/horse.png')
+    # 네번째 슬라이드
+    my_file.add('새', 'tests/bird.png')
+    my_file.save('tests/presentation_test_results/example.pptx')
